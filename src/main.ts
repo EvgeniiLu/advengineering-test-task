558a1a34d5feb231a1ad7a65cc158e7e15136be3
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-06 05:57:37 +0300

    add global registration components

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import mdiVue from 'mdi-vue/v3.js'
+import * as mdijs from '@mdi/js'
 
 import App from './App.vue'
 import router from './router'
 
+import ButtonComp from './components/ui/ButtonComp.vue'
+import CardComp from './components/ui/CardComp.vue'
+import TextFieldComp from './components/ui/TextFieldComp.vue'
+import TitleComp from './components/ui/TitleComp.vue'
+
 const app = createApp(App)
+
+app.component('button-comp', ButtonComp)
+app.component('card-comp', CardComp)
+app.component('text-field-comp', TextFieldComp)
+app.component('title-comp', TitleComp)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(mdiVue, {
+  icons: mdijs
+})
 app.mount('#app')
