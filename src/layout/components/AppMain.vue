7017b21e6c5710e07b2129d560c0b529e745043f
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-06 06:00:11 +0300

    refactoring

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <router-view v-slot="{ Component }">
+      <transition
+        name="fade-transform"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
