b0b83f29393a9836fea2e2ec61bc3cd3b1508c5b
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-06 05:57:03 +0300

    add CardComp.vue

---
+++ src/components/ui/CardComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="card_comp"
+    :style="{
+      backgroundColor: backgroundColor,
+      borderRadius: `${borderRadius}px`,
+      padding: `${padding}px`
+    }">
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    backgroundColor: {
+      type: String,
+      default: 'white'
+    },
+    borderRadius: {
+      type: [String, Number],
+      default: 5
+    },
+    padding: {
+      type: [String, Number],
+      default: 15
+    }
+  })
+</script>
+
+<style scoped>
+  .card_comp {
+    width: 100%;
+  }
+</style>
