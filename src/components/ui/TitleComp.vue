79c1185a5545d94fa0afb17868e84a3303f97583
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-06 05:55:50 +0300

    add TitleComp.vue

---
+++ src/components/ui/TitleComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="title_comp" :class="{ 'flex_start_comp': textStart }">
+    <span
+      :style="{
+        fontSize: `${fontSize}px`,
+        fontWeight: `${fontWeight}`,
+        color: color
+      }"
+    >
+      {{title}}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    fontSize: {
+      type: [String, Number],
+      default: 18
+    },
+    fontWeight: {
+      type: [String, Number],
+      default: 600
+    },
+    color: {
+      type: String,
+      default: '#000000'
+    },
+    textStart: Boolean
+  })
+</script>
+
+<style scoped>
+.title_comp {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.flex_start_comp {
+  justify-content: start;
+}
+</style>
