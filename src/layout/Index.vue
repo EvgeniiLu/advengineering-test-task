7c352f9cf851593eef8fa11d531bc17e8628e379
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-06 05:56:26 +0300

    add Index.vue

---
+++ src/layout/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="[navbarHeight]">
+    <Navbar/>
+    <AppMain class="main"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Navbar, AppMain } from "./components/index"
+  import {computed} from "vue";
+
+  const navbarHeight = computed(()=> {
+    return {
+      '--navbar-height': '80px'
+    }
+  })
+</script>
+
+<style scoped>
+.main {
+  margin-top: var(--navbar-height);
+  height: calc(100vh - var(--navbar-height));
+}
+</style>
