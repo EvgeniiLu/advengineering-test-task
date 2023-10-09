b725af38b0f5fd817dacc08613ef2e058184a27c
Lushchai Evgenii <lushayevgen@mail.ru>
2023-10-09 05:59:23 +0300

    register NotificationComponent

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import TitleComponent from './components/ui/TitleComponent.vue'
 import TabsComponent from './components/ui/TabsComponent.vue'
 import TableComponent from './components/ui/TableComponent.vue'
 import ModalComponent from './components/ui/ModalComponent.vue'
+import NotificationComponent from './components/ui/NotificationComponent.vue'
 
 const app = createApp(App)
 
@@ -24,6 +25,7 @@ app.component('title-component', TitleComponent)
 app.component('tabs-component', TabsComponent)
 app.component('table-component', TableComponent)
 app.component('modal-component', ModalComponent)
+app.component('notification-component', NotificationComponent)
 
 app.use(createPinia())
 app.use(router)
